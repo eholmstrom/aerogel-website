2e50b8acad3d26f3c8f9fa8ccc43ea86cf0150c9
Eric Holmström <eric_holmstrom@hotmail.com>
2018-08-08 11:53:37 +0200

    Fixed blog, added new favicon

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -61,14 +61,15 @@ const limit = 500
 const blogpost1 = {
   title: <h2>Do we even need wetsuits in Sweden?</h2>,
   content:
-    '34 degrees centigrade outside. Motivating yourself to develop the best insulating wetsuit on the market is not easy when this weather seems to stay forever. But we all know that sooner or later, winter is coming! \n Sometimes I just love technology and the way it can change your everyday life. We had a slow start of the week but we started using an app called Trello and I must say it had major effect on our work. As an entrepreneur, I usually suffer from having to many different thoughts on my mind. But Trello let you do smart to do lists and instead of hiring a boss telling me what to do, Trello does the work surprisingly well! \n The issue we faced last week was the difficulty in quantifying how good our material is. Thermal conductivity measurements are not as straight forward as one may think and after trying out different ways of measuring, we realized it would be a time savior to dig down in literature again. One or two days of research and we now have a decent summary of the alternatives out there. Hopefully we can have results in the near future. Too bad the whole country is on vacation though…',
+    '34\u2103 outside. Motivating yourself to develop the best insulating wetsuit on the market is not easy when this weather seems to stay forever. But we all know that sooner or later, winter is coming! \n\n Sometimes I just love technology and the way it can change your everyday life. We had a slow start of the week but we started using an app called Trello and I must say it had major effect on our work. As an entrepreneur, I usually suffer from having to many different thoughts on my mind. But Trello let you do smart to do lists and instead of hiring a boss telling me what to do, Trello does the work surprisingly well! \n The issue we faced last week was the difficulty in quantifying how good our material is. Thermal conductivity measurements are not as straight forward as one may think and after trying out different ways of measuring, we realized it would be a time savior to dig down in literature again. One or two days of research and we now have a decent summary of the alternatives out there. Hopefully we can have results in the near future. Too bad the whole country is on vacation though…',
 }
 
 const blogpost2 = {
   title: <h2>Reaching out to customers</h2>,
-  content: 'During these weeks the goal has been to ',
+  content: 'Coming soon',
 }
 
+// eslint-disable-next-line
 class Blog extends React.Component {
   render () {
     return (
@@ -80,14 +81,16 @@ class Blog extends React.Component {
         }}
       >
         <MediaQuery minDeviceWidth={769}>
-          <div className="sidePicture" />
+          <div className="sidePicture">
+            <img src={require('../assets/newviklogga.png')} alt="logo" width="229" height="130" />
+          </div>
         </MediaQuery>
         <div className="blogWrapper">
           <h1
             style={{
               color: '#3a5b6c',
               textAlign: 'center',
-              marginBottom: 30,
+              marginBottom: 50,
               marginTop: 50,
             }}
           >
@@ -95,7 +98,7 @@ class Blog extends React.Component {
           </h1>
           {blogpost1.title}
           <LongText content={blogpost1.content} limit={limit} />
-          <div className="divider" />
+          <div className="blog-divider" />
           {blogpost2.title}
           <LongText content={blogpost2.content} limit={limit} />
         </div>
